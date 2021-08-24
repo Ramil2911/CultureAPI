cce4af7eb634ffee5ad46ca177f07792ec1c41ef
ramil <ramil2911912@gmail.com>
2021-08-24 17:14:44 +0500

    Add test user account

--- MovieWebsite.Accounts/Models/Databases/AccountContext.cs
+++ MovieWebsite.Accounts/Models/Databases/AccountContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace MovieWebsite.Accounts.Models.Databases
@@ -20,16 +21,30 @@ namespace MovieWebsite.Accounts.Models.Databases
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Account>(x => x
-                .HasData(new Account
+                .HasData(new List<Account>()
                 {
-                    Id = 1,
-                    Login = "admin",
-                    Password = "admin",
-                    Mail = "ramil2911912@gmail.com",
-                    RegisterTime = DateTime.Parse("29.11.04"),
-                    LastOnlineTime = DateTime.Now,
-                    AvatarId = 0,
-                    Role = "admin"
+                    new Account
+                    {
+                        Id = 1,
+                        Login = "admin",
+                        Password = "admin",
+                        Mail = "ramil2911912@gmail.com",
+                        RegisterTime = DateTime.Parse("29.11.04"),
+                        LastOnlineTime = DateTime.Now,
+                        AvatarId = 0,
+                        Role = "admin"
+                    },
+                    new Account
+                    {
+                        Id = 2,
+                        Login = "user",
+                        Password = "user",
+                        Mail = "ramil2911912@gmail.com",
+                        RegisterTime = DateTime.Parse("29.11.04"),
+                        LastOnlineTime = DateTime.Now,
+                        AvatarId = 0,
+                        Role = "user"
+                    }
                 }));
             
         }
