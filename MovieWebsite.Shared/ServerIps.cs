b4f837e91623fb69a137bb35427f9d9e82062bc5
ramil <ramil2911912@gmail.com>
2021-08-24 17:12:51 +0500

    Add server ips

--- MovieWebsite.Shared/ServerIps.cs
+++ MovieWebsite.Shared/ServerIps.cs
@@ -1,7 +1,16 @@
 ﻿namespace MovieWebsite.Shared
 {
-    public class ServerIps
+    public static class ServerIps
     {
-        
+        public static string[] Value = new[]
+        {
+            "https://localhost:5001", //Accounts
+            "https://localhost:5011", //Movies
+            "https://localhost:5021", //Characters
+            "https://localhost:5031", //Persons
+            "https://localhost:5051", //Images
+            "https://localhost:5071", //Ratings
+            "https://localhost:5081" //Blogs
+        };
     }
 }
